676ed0ebd2cc97f6377dcef166f25001f3610343
Thwih <hoanguyeen33@gmail.com>
2026-07-02 19:43:40 +0700

    Delete bottom.js

--- src/components/bottom.js
+++
@@ -1,8 +0,0 @@
-export function renderBottom() {
-  return `
-    <div class="bottom-group">
-      <div class="time-widget"><i class="fas fa-clock"></i><span class="time-display" id="clockDisplay">00:00:00</span><span class="time-label" data-i18n="current_time">Giờ hiện tại</span></div>
-      <div class="footer" data-i18n="footer"><i class="fas fa-code"></i> Designed by <a href="#">Thwih</a> &bull; 2026 &nbsp;&nbsp;|&nbsp;&nbsp; <i class="fas fa-globe"></i> thwih.com</div>
-    </div>
-  `;
-}
