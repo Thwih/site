aa6156a2977af45b85eed6698b9e7e4524d90741
Thwih <hoanguyeen33@gmail.com>
2026-07-02 19:23:57 +0700

    Create src/components/menu.js

---
+++ src/components/menu.js
@@ -0,0 +1,26 @@
+export function renderMenu() {
+  return `
+    <div class="menu-modern">
+      <a href="https://www.tiktok.com/@thwih1" target="_blank" class="menu-item">
+        <span class="icon-wrap"><i class="fab fa-tiktok"></i></span>
+        <span class="info"><span class="title" data-i18n="menu_follow_tiktok">Theo dõi TikTok</span><span class="desc"> @thwih1</span></span>
+        <span class="arrow"><i class="fas fa-chevron-right"></i></span>
+      </a>
+      <a href="https://t.me/thwihmusic" target="_blank" class="menu-item">
+        <span class="icon-wrap"><i class="fab fa-telegram-plane"></i></span>
+        <span class="info"><span class="title" data-i18n="menu_telegram">Telegram</span><span class="desc"> @thwihmusic</span></span>
+        <span class="arrow"><i class="fas fa-chevron-right"></i></span>
+      </a>
+      <a href="https://zalo.me/g/otroe2pxpnitzfr6hppg" target="_blank" class="menu-item">
+        <span class="icon-wrap"><i class="fas fa-comment-dots"></i></span>
+        <span class="info"><span class="title" data-i18n="menu_zalo">Zalo Group</span><span class="desc" data-i18n="menu_zalo_desc">Cộng đồng âm nhạc</span></span>
+        <span class="arrow"><i class="fas fa-chevron-right"></i></span>
+      </a>
+      <a href="https://zalo.me/84338578255" target="_blank" class="menu-item">
+        <span class="icon-wrap"><i class="fas fa-phone"></i></span>
+        <span class="info"><span class="title" data-i18n="menu_contact">Liên hệ</span><span class="desc" data-i18n="menu_contact_desc">Hợp tác &amp; hỗ trợ</span></span>
+        <span class="arrow"><i class="fas fa-chevron-right"></i></span>
+      </a>
+    </div>
+  `;
+}
